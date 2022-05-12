3965da39dd0999ae45ccd63f69af8dabe0c26818
Dirk Chang <dirk@kooiot.com>
2022-05-12 17:13:50 +0800

    using /admin as the static pages

--- server/api/router.go
+++ server/api/router.go
@@ -1,7 +1,8 @@
 package api
 
 import (
-	"github.com/gin-contrib/static"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kooiot/robot/server/api/middleware"
 	"github.com/kooiot/robot/server/config"
@@ -11,8 +12,10 @@ import (
 
 func Routers(conf *config.HttpApiConf) *gin.Engine {
 	var Router = gin.Default()
-	// Router.StaticFS("/", http.Dir(conf.Static)) // 静态地址
-	Router.Use(static.Serve("/", static.LocalFile(conf.Static, false)))
+	Router.StaticFS("/admin/", http.Dir(conf.Static)) // 静态地址
+
+	// "github.com/gin-contrib/static"
+	// Router.Use(static.Serve("/", static.LocalFile(conf.Static, false)))
 	// 方便统一添加路由组前缀 多服务器上线使用
 
 	PublicGroup := Router.Group("api")
